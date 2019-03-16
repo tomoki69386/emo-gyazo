7bcff753063dfb997bea7d938d3a65cf9251cdcf
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-17 03:00:34 +0900

    Update APpDelegate

--- emo-gyazo/AppDelegate.swift
+++ emo-gyazo/AppDelegate.swift
@@ -18,6 +18,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().barTintColor = UIColor(named: "main")
         UINavigationBar.appearance().tintColor = .white
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        // .envの読み込み
+        guard let path = Bundle.main.path(forResource: ".env", ofType: nil) else {
+            fatalError(".envファイルがプロジェクトファイルに存在しません。")
+        }
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            let str = String(data: data, encoding: .utf8) ?? "Empty File"
+            let clean = str.replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: "'", with: "")
+            let envVars = clean.components(separatedBy:"\n")
+            for envVar in envVars {
+                let keyVal = envVar.components(separatedBy:"=")
+                if keyVal.count == 2 {
+                    setenv(keyVal[0], keyVal[1], 1)
+                }
+            }
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        
         return true
     }
 
